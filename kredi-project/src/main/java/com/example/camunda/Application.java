4959316a35626f1ba5b083e79aac4fb0c018f246
mertmahanoglu <72344057+mertmahanoglu@users.noreply.github.com>
2022-09-30 10:15:28 +0300

    Update Application.java

--- kredi-project/src/main/java/com/example/camunda/Application.java
+++ kredi-project/src/main/java/com/example/camunda/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableProcessApplication("asd")
+@EnableProcessApplication("asd")
 public class Application {
 
   public static void main(String... args) {
